ffe26bdc66f1c262b9adb691d00de0f6f4073e3a
Dillan Smith <smithd2@allegheny.edu>
2018-01-11 14:22:52 -0500

    clean red pencil. start testing

---
+++ tests/RedPencilProductTest.java
@@ -0,0 +1,16 @@
+import org.junit.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+
+public class RedPencilProductTest {
+    private RedPencilProduct red;
+
+    @Before
+    public void createRedPencilProduct() {
+        red = new RedPencilProduct();
+    }
+    @Test
+    public void whenCheckIn30TurnsOffRedPencilSale(){
+
+    }
+}
