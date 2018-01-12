289246af51f8e4b4d5a2423bf60698b41c6a812a
Dillan Smith <smithd2@allegheny.edu>
2018-01-12 11:36:46 -0500

    passes handling commas and new lines as delimiters

--- src/NewStringCalculator.java
+++ src/NewStringCalculator.java
@@ -7,12 +7,13 @@ public class NewStringCalculator {
 
     protected int calculateStringValue(String num){
         if(num == null || num.equals("")) return 0;
-        num = num.replaceAll(" ", "");
-        String[] nums = num.split(",");
+
+        String[] nums = num.split(",|\n");
         int sum = 0;
         for(String x: nums){
             sum += Integer.parseInt(x);
         }
+        System.out.println(sum);
         return sum;
     }
 }
