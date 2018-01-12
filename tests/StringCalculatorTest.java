e854f0ed1638238dd4430c44ef5cd9a2f4789972
Dillan Smith <smithd2@allegheny.edu>
2018-01-12 11:33:10 -0500

    passes handling many numbers, the tests randomizes 50 nums and tests

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -1,4 +1,7 @@
 import org.junit.*;
+
+import java.util.Random;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
@@ -23,6 +26,24 @@ public class StringCalculatorTest {
     @Test
     public void givenTwoNumsReturnsNumericValue(){
         assertThat(3, equalTo(calc.calculateStringValue("1,2")));
+    }
+
+    @Test public void handlesCommasAndNewLinesAsDelimiters(){
+        assertThat(6, equalTo(calc.calculateStringValue("1,2\n3")));
+    }
+    @Test public void givenManyNumsReturnsNumericValue(){
+        String num = "";
+        int sum = 0;
+        String prefix = "";
+        Random r = new Random();
+        for(int i = 0; i < 50; i++){
+            int x = r.nextInt(100);
+            sum += x;
+            num += prefix + Integer.toString(x);
+            prefix = ",";
+        }
+
+        assertThat(sum, equalTo(calc.calculateStringValue(num)));
     }
 
 
